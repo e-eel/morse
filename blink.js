4745bc9e26ff3d234dbafcd6d13755572ab585c1
Nils Muenchhausen <nils@muenchhausen.de>
2018-02-11 20:51:37 +0100

    morse alphabet

--- blink.js
+++ blink.js
@@ -1,14 +1,33 @@
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 
 
-console.log("args: "+process.argv);
+
 let arguments = process.argv.slice(2);
 if (!arguments.length) {
-    console.log("expected wait");
+    console.log("expected message");
     process.exit(1);
 }
-let wait = arguments[0];
+var morseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",     ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
 
+let message = arguments[0];
+message = message.toLowerCase().replace(/[^a-z]/g, "");
+for (let i=0; i<message.length; i++) {
+  let c = message.charCodeAt(i)-97;  
+  console.log(c);
+  let mCode = morseCode[c];
+  console.log(mCode);
+}
+
+console.log(": "+message);
+function short() {
+  process.stdout.write(".");
+}
+function long() {
+  process.stdout.write("-");
+}
+short();short();short();long();long();long();short();short();short();
+
+/*
 var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
 var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
 
@@ -28,3 +47,4 @@ function endBlink() { //function to stop blinking
 }
 
 setTimeout(endBlink, 5000); //stop blinking after 5 seconds
+*/

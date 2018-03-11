080bf14244604254208b90b59a6d11d4ad96cd6a
Nils Muenchhausen <nils@muenchhausen.de>
2018-03-11 11:44:14 +0100

    sos with different timings working

--- listen.js
+++ listen.js
@@ -1,22 +1,31 @@
 
 let morseAlpha = new Map();
+//morseAlpha.set("-.","n");
+//morseAlpha.set("..","i");
+//morseAlpha.set(".-..","l");
 morseAlpha.set("...","s");
 morseAlpha.set("---","o");
 let morseStream= "";
-function newSignal(length) {
-    if (length==100) {
+function newSignal( lengthOff, lengthOn ) {
+    let blanc= "";
+    if ( Math.abs(lengthOff-700) < 70  ) { 
+        blanc= " ";
+
+    }
+    if ( Math.abs(lengthOn-100) < 10 ) {
         morseStream+=".";
     } else {
         morseStream+="-";
     }
+   
    let chr= morseAlpha.get(morseStream);
    if (chr!=undefined) {
        console.log(chr);
        morseStream= "";
-       return chr;
+       return blanc+chr;
    } 
 
-   return "";
+   return blanc;
 }
 
 
--- spec/listen.spec.js
+++ spec/listen.spec.js
@@ -5,21 +5,49 @@ describe("listen morse", function() {
   });
 
   it("should be able to listen to sos with exact timings", function() {
-    let onOff=[+100,100,+100,100,+100,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100,700];
+    let onOff=[700,+100,100,+100,100,+100,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100];
     let result = "";
     for(i=0;i<onOff.length;i+=2){
-      result += newSignal(onOff[i]);
+      result += newSignal(onOff[i],onOff[ i+1 ]);
     }
-    expect(result).toBe("sos");
+    expect(result).toBe(" sos");
   });
-/*
+
   it("should be able to listen to sos with aproximate timings", function() {
-    let onOff=[+95,100,+105,100,+98,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100,700];
+    let onOff=[700,+95,100,+105,100,+98,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100];
     let result = "";
     for(i=0;i<onOff.length;i+=2){
-      result += newSignal(onOff[i]);
+      result += newSignal(onOff[i],onOff[ i+1 ]);
     }
-    expect(result).toBe("sos");
+    expect(result).toBe(" sos");
   });
-*/
+
+  it("should be able to listen to sos sos with exact timings", function() {
+    let onOff=[700,+100,100,+100,100,+100,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100,
+               700,+100,100,+100,100,+100,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100];
+    let result = "";
+    for(i=0;i<onOff.length;i+=2){
+      result += newSignal(onOff[i],onOff[ i+1 ]);
+    }
+    expect(result).toBe(" sos sos");
+  });
+  it("should be able to listen to sos sos with aproximate timings", function() {
+    let onOff=[702,+100,100,+100,100,+100,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100,
+               680,+100,100,+100,100,+100,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100];
+    let result = "";
+    for(i=0;i<onOff.length;i+=2){
+      result += newSignal(onOff[i],onOff[ i+1 ]);
+    }
+    expect(result).toBe(" sos sos");
+  });
+  /*
+  it("should be able to listen to nils with exact timings", function() {
+    let onOff=[700,+300,100,+100];
+    let result = "";
+    for(i=0;i<onOff.length;i+=2){
+      result += newSignal(onOff[i],onOff[ i+1 ]);
+    }
+    expect(result).toBe(" n");
+  });
+  */
 });

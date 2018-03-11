441843c7add216f48b4ee2bcbf5aa8162231e97b
Nils Muenchhausen <nils@muenchhausen.de>
2018-03-11 12:28:38 +0100

    char n is working

--- listen.js
+++ listen.js
@@ -1,31 +1,38 @@
 
 let morseAlpha = new Map();
-//morseAlpha.set("-.","n");
+morseAlpha.set("-.","n");
 //morseAlpha.set("..","i");
-//morseAlpha.set(".-..","l");
+morseAlpha.set(".-..","l");
 morseAlpha.set("...","s");
 morseAlpha.set("---","o");
 let morseStream= "";
 function newSignal( lengthOff, lengthOn ) {
-    let blanc= "";
-    if ( Math.abs(lengthOff-700) < 70  ) { 
-        blanc= " ";
-
+    if ( lengthOff >= 1500*0.9 ) { 
+        morseStream= "";
     }
+
+    let recognizedCharacters = "";
+    if ( lengthOff >= 700*0.9 ) { 
+        recognizedCharacters= " ";
+    }
+   
+    if (  lengthOff >= 300*0.9 ) {
+        let chr= morseAlpha.get(morseStream);
+        if (chr!=undefined) {
+            console.log(chr);
+            morseStream= "";
+            recognizedCharacters = chr+recognizedCharacters;
+        } 
+    }
+
     if ( Math.abs(lengthOn-100) < 10 ) {
         morseStream+=".";
     } else {
         morseStream+="-";
     }
    
-   let chr= morseAlpha.get(morseStream);
-   if (chr!=undefined) {
-       console.log(chr);
-       morseStream= "";
-       return blanc+chr;
-   } 
-
-   return blanc;
+   
+   return recognizedCharacters;
 }
 
 
--- spec/listen.spec.js
+++ spec/listen.spec.js
@@ -2,10 +2,11 @@ describe("listen morse", function() {
   let newSignal = require('../listen');
   
   beforeEach(function() {
+    let morseStream= "";
   });
 
   it("should be able to listen to sos with exact timings", function() {
-    let onOff=[700,+100,100,+100,100,+100,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100];
+    let onOff=[1500,+100,100,+100,100,+100, 300,+300,100,+300,100,+300, 300,+100,100,+100,100,+100, 300,0 ];
     let result = "";
     for(i=0;i<onOff.length;i+=2){
       result += newSignal(onOff[i],onOff[ i+1 ]);
@@ -14,7 +15,7 @@ describe("listen morse", function() {
   });
 
   it("should be able to listen to sos with aproximate timings", function() {
-    let onOff=[700,+95,100,+105,100,+98,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100];
+    let onOff=[1500,+95,100,+105,100,+98, 300,+300,100,+300,100,+300, 300,+100,100,+100,100,+100, 300,0];
     let result = "";
     for(i=0;i<onOff.length;i+=2){
       result += newSignal(onOff[i],onOff[ i+1 ]);
@@ -22,32 +23,32 @@ describe("listen morse", function() {
     expect(result).toBe(" sos");
   });
 
-  it("should be able to listen to sos sos with exact timings", function() {
-    let onOff=[700,+100,100,+100,100,+100,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100,
-               700,+100,100,+100,100,+100,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100];
+  it("should be able to listen to sossos with exact timings", function() {
+    let onOff=[1500,+100,100,+100,100,+100, 300,+300,100,+300,100,+300, 300,+100,100,+100,100,+100,
+               300,+100,100,+100,100,+100, 300,+300,100,+300,100,+300, 300,+100,100,+100,100,+100, 300,0];
     let result = "";
     for(i=0;i<onOff.length;i+=2){
       result += newSignal(onOff[i],onOff[ i+1 ]);
     }
-    expect(result).toBe(" sos sos");
+    expect(result).toBe(" sossos");
   });
   it("should be able to listen to sos sos with aproximate timings", function() {
-    let onOff=[702,+100,100,+100,100,+100,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100,
-               680,+100,100,+100,100,+100,100,+300,100,+300,100,+300,100,+100,100,+100,100,+100];
+    let onOff=[1500,+100,100,+100,100,+100, 300,+300,100,+300,100,+300, 300,+100,100,+100,100,+100,
+               680,+100,100,+100,100,+100, 300,+300,100,+300,100,+300, 300,+100,100,+100,100,+100, 300,0];
     let result = "";
     for(i=0;i<onOff.length;i+=2){
       result += newSignal(onOff[i],onOff[ i+1 ]);
     }
     expect(result).toBe(" sos sos");
   });
-  /*
-  it("should be able to listen to nils with exact timings", function() {
-    let onOff=[700,+300,100,+100];
+  
+  it("should be able to listen to n with exact timings", function() {
+    let onOff=[1500,+300,100,+100, 300,0];
     let result = "";
     for(i=0;i<onOff.length;i+=2){
       result += newSignal(onOff[i],onOff[ i+1 ]);
     }
     expect(result).toBe(" n");
   });
-  */
+  
 });
